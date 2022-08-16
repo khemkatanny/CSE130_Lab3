7671c2936166cd9b31e47184ac5420800dbdaa78
tanny <50891864+khemkatanny@users.noreply.github.com>
2022-08-16 11:50:39 -0400

    Add files via upload

---
+++ condvar.h
@@ -0,0 +1,18 @@
+#ifndef THREADS_CONDVAR_H
+#define THREADS_CONDVAR_H
+
+#include <list.h>
+#include <stdbool.h>
+#include "threads/lock.h"
+
+/* Condition variable. */
+struct condvar {
+  struct list waiters;   /* List of semaphores with waiting threads. */
+};
+
+void condvar_init (struct condvar *);
+void condvar_wait (struct condvar *, struct lock *);
+void condvar_signal (struct condvar *, struct lock *);
+void condvar_broadcast (struct condvar *, struct lock *);
+
+#endif /* threads/CONDVAR.h */
---
+++ lock.h
@@ -0,0 +1,19 @@
+#ifndef THREADS_LOCK_H
+#define THREADS_LOCK_H
+
+#include <list.h>
+#include <stdbool.h>
+#include "threads/semaphore.h"
+
+/* A Lock */
+struct lock {
+  struct thread *holder;      /* Thread holding lock */
+  struct semaphore semaphore; /* Binary semaphore controlling access. */
+};
+
+void lock_init (struct lock *);
+void lock_acquire (struct lock *);
+void lock_release (struct lock *);
+bool lock_held_by_current_thread (const struct lock *);
+
+#endif /* threads/LOCK.h */
